e5dd0b6c46cce7006341fee9770550335fb17d0f
Getter44 <max.molling@gmail.com>
2022-04-04 12:17:29 +0200

    :1234:  Websitev2

--- tourify/src/screens/Documentation.jsx
+++ tourify/src/screens/Documentation.jsx
@@ -9,40 +9,40 @@ class Documentation extends Component {
   render() {
     return (
       <div className="flex justify-center">
-        <div className="w-5/6 h-full column-1 bg-[#4053A9] rounded-lg flex-col flex">
-          <p className="text-center text-5xl text-white mb-5">Anleitung zur Erstellung eigener Touren</p>
+        <div className="w-full h-full column-1 bg-[#4053A9] rounded-lg flex-col flex ">
+          <p className="text-center text-5xl text-white mb-5 mt-3">Anleitung zur Erstellung eigener Touren</p>
 
-          <p className='text-center text-white mb-3'>Um eine Tour zu erstellen benötigt man zuerst einen Account. Dafür klickt man auf der 
-            Homepage auf <a className='underline text-black' href='http://localhost:3000/login'>get started</a> </p>
+          <div className="w-1/2 self-center">
 
-          <div className="w-1/2 self-center mb-3">
             <img className="object-scale-down rounded-lg shadow-lg" src={tut1} alt="Hauptseite" />
+            <p className='text-white mb-3'>Bild 1.1 - Homepage</p>
+            <p className='text-white text-2xl mb-3'>Um eine Tour zu erstellen benötigt man zuerst einen Account. Dafür klickt man auf der 
+            Homepage auf <a className='underline text-cyan-100' href='http://localhost:3000/login'>get started.</a> </p>
+
+            <img className="rounded-lg mt-10" src={tut2} alt="Account erstellungs Seite" />
+            <p className='text-white mb-3'>Bild 1.2 - Log in page</p>
+            <p className="text-white mb-3 text-2xl">Hier kann man sich bei seinem bestehenden Account einloggen oder einen neuen erstellen.</p>
+
+            <img className="rounded-lg mt-10" src={tut3} alt="Project listing Seite" />
+            <p className='text-white mb-3'>Bild 1.3 - Projects page</p>
+            <p className="text-white mb-3 text-2xl">Anschließend sieht man das Hauptmenü mit allen bereits erstellten Touren. <br />
+            Momentan ist sie noch leer. Mit <a className='underline text-cyan-100' href="">Tour erstellen</a> kann man eine Neue hinzufügen.</p>
+
+
+            <img className="rounded-lg mt-10" src={tut4} alt="Editor" />
+            <p className='text-white mb-3'>Bild 1.4 - Editor page</p>
+            <p className="text-white mb-3 text-2xl">Sobald man ein Projekt erstellt hat, kann man dieses im Editor bearbeiten. <br /> 
+              Hier kann man Elemente wie Bilder, Videos und Audio-Dateien per drag and drop hinzufügen. 
+            </p>
+
+
+            <img className="rounded-lg mt-10" src={tut4} alt="Editor" />
+            <p className='text-white mb-3'>Bild 1.4 - Editor page</p>
+            <p className="text-white mb-3 text-2xl"> Hier ist eine Beispielseite für ein Museum erstellt worden. <br /> 
+              Anschließend kann man die Station abspeichern und ein QR-Code wird generiert, welcher auf diese Seite verweißt.
+            </p>
+            
           </div>
-
-          <p className="text-center text-white mb-3">Hier kann man sich bei seinem bestehenden Account einloggen oder einen neuen erstellen</p>
-
-          <div className="w-1/2 self-center mb-3">
-            <img className="rounded-lg shadow-lg" src={tut2} alt="Account erstellungs Seite" />
-          </div>
-
-          <p className="text-center text-white mb-3">Anschließend sieht man das Hauptmenü mit allen bereits erstellten Touren.
-          Momentan ist sie noch leer. Mit <a className='underline text-cyan' href="">Tour erstellen</a> kann man eine neue hinzufügen</p>
-
-          <div className="w-1/2 self-center mb-3">
-            <img className="rounded-lg" src={tut3} alt="Project listing Seite" />
-          </div>
-
-          <p className="text-center text-white mb-3">Sobald man ein Projekt erstellt hat, kann man dieses im Editor bearbeiten. <br /> 
-            Hier kann man Elemente wie Bilder, Videos und Audio-Dateien per drag and drop hinzufügen. 
-          </p>
-
-          <div className="w-1/2 self-center mb-3">
-            <img className="rounded-lg" src={tut4} alt="Editor" />
-          </div>
-
-          <p className="text-center text-white mb-3"> Hier ist eine Beispielseite für ein Museum erstellt worden. <br /> 
-            Anschließend kann man die Station abspeichern und ein QR code wird generiert, welche zu dieser Seite verweißt.
-          </p>
 
         </div>
 
