e6a9250b614ce5e81dfcbf3876d45646db768d63
kaffarell <gabrielgoller123@gmail.com>
2022-04-04 11:16:05 +0200

    :fire: Removed upper-case files

--- tourify/src/models/editor/TextProperties.js
+++
@@ -1,9 +0,0 @@
-import { getContainerProperties, getFontProperties, getLayoutProperties } from "./properties";
-
-export const getTextProperties = () => _textProperties;
-
-const _textProperties = {
-    ...getContainerProperties(),
-    ...getFontProperties(),
-    ...getLayoutProperties(),
-}
